a06305d8ec4d9ebdaececc60b787ae323da92329
pratik8696 <mepratikanand@gmail.com>
2021-08-06 23:21:37 +0530

    homepage done

---
+++ src/components/Product.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Card} from "react-bootstrap";
+function Product(props){
+    return (
+        <div>
+            <Card className="m-3 p-3 rounded">
+                <a href={`/product/${props.product._id}`}>
+                    <Card.Img src={props.product.image} variant="top" >
+                    </Card.Img>
+                </a>
+            
+            <Card.Body>
+                    <Card.Title as='div'>
+                        <strong>
+                            {props.product.name}
+                        </strong>
+                    </Card.Title>
+            </Card.Body>
+            <Card.Text as="div">
+                    <Card.Title className="ms-3">
+                        {props.product.rating} out of {props.product.numReviews} reviews
+                    </Card.Title>
+            </Card.Text>
+            <Card.Text as="h3" className="ms-2">
+                $ {props.product.price}
+            </Card.Text>
+            </Card>
+        </div>
+    )
+}
+
+export default Product
